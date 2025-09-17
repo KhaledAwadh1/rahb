b6cf31fdb114dbf36e2f054b460e8f53d3b70198
KhaledAwadh1 <khaled22abdullah22@gmail.com>
2025-09-17 17:43:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -198,3 +198,12 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+const slides = document.querySelectorAll('.hero img');
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}, 2000); // كل 2 ثانية
